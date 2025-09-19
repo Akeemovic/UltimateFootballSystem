647ba7a3cc86c90e50391bd719eb4f6700c9c9f2
Akeemovic <akeemovic26@gmail.com>
2025-09-19 16:02:10 +0400

    feat(PlayerItemViewSelectionLayer): add selectability control for non-starting players
    Add Update method to control selectability based on player presence
    Implement SetSelectability with visual feedback and logging
    Add helper methods for enabling/disabling selectability

--- Gameplay/Tactics/Scripts/TacticBoard/Player/PlayerItemViewSelectionLayer.cs
+++ Gameplay/Tactics/Scripts/TacticBoard/Player/PlayerItemViewSelectionLayer.cs
@@ -1,342 +1,4 @@
-// using UnityEngine;
-// using UnityEngine.EventSystems;
-// using UnityEngine.UI;
-//
-// namespace UltimateFootballSystem.Gameplay.Tactics
-// {
-//     public class PlayerItemViewSelectionLayer : Selectable, IPointerClickHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
-//     {
-//         [SerializeField] 
-//         private PlayerItemView playerItemView;
-//
-//         private TacticsBoardController _controller;
-//         private ScrollRect parentScrollRect;
-//         private Vector2 pointerDownPos;
-//         private float pointerDownTime;
-//         private bool isDragging = false;
-//         private bool isPointerClick = false;
-//         private const float MAX_TAP_TIME = 0.2f;
-//         private const float MAX_TAP_DISTANCE = 10f;
-//
-//         // Lazy initialization for controller
-//         private TacticsBoardController controller
-//         {
-//             get
-//             {
-//                 if (_controller == null && playerItemView != null)
-//                 {
-//                     _controller = playerItemView.Controller;
-//                 }
-//                 return _controller;
-//             }
-//         }
-//
-//         protected override void Awake()
-//         {
-//             base.Awake();
-//             
-//             // Try to get playerItemView if not set in inspector
-//             if (playerItemView == null)
-//             {
-//                 playerItemView = GetComponent<PlayerItemView>();
-//             }
-//             
-//             parentScrollRect = GetComponentInParent<ScrollRect>();
-//
-//             if (Application.isMobilePlatform)
-//             {
-//                 transition = Transition.None;
-//             }
-//         }
-//
-//         public override void OnSelect(BaseEventData eventData)
-//         {
-//             base.OnSelect(eventData);
-//             
-//             // Only play selection sound if this wasn't triggered by a pointer click
-//             if (!isPointerClick && eventData is not PointerEventData && controller != null)
-//             {
-//                 controller.PlaySelectSound();
-//             }
-//             
-//             LogSelectionState("Selected");
-//         }
-//
-//         public void OnBeginDrag(PointerEventData eventData)
-//         {
-//             isDragging = true;
-//             if (parentScrollRect != null)
-//             {
-//                 parentScrollRect.OnBeginDrag(eventData);
-//             }
-//         }
-//
-//         public void OnDrag(PointerEventData eventData)
-//         {
-//             if (parentScrollRect != null)
-//             {
-//                 parentScrollRect.OnDrag(eventData);
-//             }
-//         }
-//
-//         public void OnEndDrag(PointerEventData eventData)
-//         {
-//             isDragging = false;
-//             if (parentScrollRect != null)
-//             {
-//                 parentScrollRect.OnEndDrag(eventData);
-//             }
-//         }
-//
-//         public override void OnPointerDown(PointerEventData eventData)
-//         {
-//             base.OnPointerDown(eventData);
-//             pointerDownPos = eventData.position;
-//             pointerDownTime = Time.time;
-//             isPointerClick = true;
-//         }
-//
-//         public override void OnPointerUp(PointerEventData eventData)
-//         {
-//             base.OnPointerUp(eventData);
-//             isPointerClick = false;
-//         }
-//
-//         public void OnPointerClick(PointerEventData eventData)
-//         {
-//             // Check if this was a tap (not a drag)
-//             if (Time.time - pointerDownTime <= MAX_TAP_TIME && 
-//                 Vector2.Distance(eventData.position, pointerDownPos) <= MAX_TAP_DISTANCE &&
-//                 !isDragging)
-//             {
-//                 HandleItemClick();
-//             }
-//         }
-//
-//         private void HandleItemClick()
-//         {
-//             // Check if controller is available
-//             if (controller == null)
-//             {
-//                 Debug.LogWarning("Controller is not available yet");
-//                 return;
-//             }
-//             
-//             // Play click sound
-//             controller.PlayClickSound();
-//             
-//             // Check if we should show role dialog
-//             if (playerItemView != null && 
-//                 playerItemView.mainView != null &&
-//                 playerItemView.mainView.ViewMode == PlayerItemViewModeOption.Roles && 
-//                 playerItemView.HasPlayerItem)
-//             {
-//                 if (controller.roleSelectorDialog != null)
-//                 {
-//                     var dialog = controller.roleSelectorDialog.Clone();
-//                     dialog.Show();
-//                     Debug.Log("Showing role dialog", dialog);
-//                 }
-//                 else
-//                 {
-//                     Debug.LogWarning("Role selector dialog is not set");
-//                 }
-//             }
-//             else
-//             {
-//                 // Handle selection/swap
-//                 controller.HandleItemClicked(playerItemView);
-//             }
-//         }
-//
-//         private void LogSelectionState(string state)
-//         {
-//             if (playerItemView != null && controller != null)
-//             {
-//                 int teamId = controller.teamId;
-//                 Debug.Log($"{state}: TeamID: {teamId}, Player: {playerItemView.Profile?.Name ?? "Empty"}");
-//             }
-//         }
-//     }
-// }
-
-// using UnityEngine;
-// using UnityEngine.EventSystems;
-// using UnityEngine.UI;
-//
-// namespace UltimateFootballSystem.Gameplay.Tactics
-// {
-//     public class PlayerItemViewSelectionLayer : Selectable, IPointerClickHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
-//     {
-//         [SerializeField] 
-//         private PlayerItemView playerItemView;
-//
-//         private TacticsBoardController _controller;
-//         private ScrollRect parentScrollRect;
-//         private Vector2 pointerDownPos;
-//         private float pointerDownTime;
-//         private bool isDragging = false;
-//         private bool isPointerClick = false;
-//         private bool hasStartedDrag = false; // Track if we've started a drag
-//         private const float MAX_TAP_TIME = 0.2f;
-//         private const float MAX_TAP_DISTANCE = 10f;
-//
-//         // Lazy initialization for controller
-//         private TacticsBoardController controller
-//         {
-//             get
-//             {
-//                 if (_controller == null && playerItemView != null)
-//                 {
-//                     _controller = playerItemView.Controller;
-//                 }
-//                 return _controller;
-//             }
-//         }
-//
-//         protected override void Awake()
-//         {
-//             base.Awake();
-//             
-//             // Try to get playerItemView if not set in inspector
-//             if (playerItemView == null)
-//             {
-//                 playerItemView = GetComponent<PlayerItemView>();
-//             }
-//             
-//             parentScrollRect = GetComponentInParent<ScrollRect>();
-//
-//             if (Application.isMobilePlatform)
-//             {
-//                 transition = Transition.None;
-//             }
-//         }
-//
-//         public override void OnSelect(BaseEventData eventData)
-//         {
-//             base.OnSelect(eventData);
-//             
-//             // Only play selection sound if this wasn't triggered by a pointer click
-//             if (!isPointerClick && eventData is not PointerEventData && controller != null)
-//             {
-//                 controller.PlaySelectSound();
-//             }
-//             
-//             LogSelectionState("Selected");
-//         }
-//
-//         public void OnBeginDrag(PointerEventData eventData)
-//         {
-//             isDragging = true;
-//             hasStartedDrag = true;
-//             
-//             // Pass through to scroll rect
-//             if (parentScrollRect != null)
-//             {
-//                 parentScrollRect.OnBeginDrag(eventData);
-//             }
-//             
-//             // Let the drag support handle the drag naturally
-//             // It will receive this event through Unity's event system
-//         }
-//
-//         public void OnDrag(PointerEventData eventData)
-//         {
-//             // Pass through to scroll rect
-//             if (parentScrollRect != null)
-//             {
-//                 parentScrollRect.OnDrag(eventData);
-//             }
-//             
-//             // Let the drag support handle the drag naturally
-//         }
-//
-//         public void OnEndDrag(PointerEventData eventData)
-//         {
-//             isDragging = false;
-//             
-//             // Pass through to scroll rect
-//             if (parentScrollRect != null)
-//             {
-//                 parentScrollRect.OnEndDrag(eventData);
-//             }
-//             
-//             // Let the drag support handle the drag naturally
-//         }
-//
-//         public override void OnPointerDown(PointerEventData eventData)
-//         {
-//             base.OnPointerDown(eventData);
-//             pointerDownPos = eventData.position;
-//             pointerDownTime = Time.time;
-//             isPointerClick = true;
-//             hasStartedDrag = false;
-//         }
-//
-//         public override void OnPointerUp(PointerEventData eventData)
-//         {
-//             base.OnPointerUp(eventData);
-//             isPointerClick = false;
-//         }
-//
-//         public void OnPointerClick(PointerEventData eventData)
-//         {
-//             // Only handle click if we haven't started dragging
-//             if (!hasStartedDrag &&
-//                 Time.time - pointerDownTime <= MAX_TAP_TIME && 
-//                 Vector2.Distance(eventData.position, pointerDownPos) <= MAX_TAP_DISTANCE)
-//             {
-//                 HandleItemClick();
-//             }
-//         }
-//
-//         private void HandleItemClick()
-//         {
-//             // Check if controller is available
-//             if (controller == null)
-//             {
-//                 Debug.LogWarning("Controller is not available yet");
-//                 return;
-//             }
-//             
-//             // Play click sound
-//             controller.PlayClickSound();
-//             
-//             // Check if we should show role dialog
-//             if (playerItemView != null && 
-//                 playerItemView.mainView != null &&
-//                 playerItemView.mainView.ViewMode == PlayerItemViewModeOption.Roles && 
-//                 playerItemView.HasPlayerItem)
-//             {
-//                 if (controller.roleSelectorDialog != null)
-//                 {
-//                     var dialog = controller.roleSelectorDialog.Clone();
-//                     dialog.Show();
-//                     Debug.Log("Showing role dialog", dialog);
-//                 }
-//                 else
-//                 {
-//                     Debug.LogWarning("Role selector dialog is not set");
-//                 }
-//             }
-//             else
-//             {
-//                 // Handle selection/swap
-//                 controller.HandleItemClicked(playerItemView);
-//             }
-//         }
-//
-//         private void LogSelectionState(string state)
-//         {
-//             if (playerItemView != null && controller != null)
-//             {
-//                 int teamId = controller.teamId;
-//                 Debug.Log($"{state}: TeamID: {teamId}, Player: {playerItemView.Profile?.Name ?? "Empty"}");
-//             }
-//         }
-//     }
-// }
-
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -375,11 +37,22 @@ namespace UltimateFootballSystem.Gameplay.Tactics
             }
         }
 
+        private void Update()
+        {
+            // Ensure non-starting list items are selectable only if they have a player
+            if (playerItemView.ViewOwnerOption != PlayerItemViewOwnerOption.StartingList)
+            {
+                SetSelectability(playerItemView.HasPlayerItem);
+            }
+        }
+
         public override void OnSelect(BaseEventData eventData)
         {
+            if (!interactable) return;
+
             base.OnSelect(eventData);
             LogSelectionState("Selected");
-        
+
             // Playing selection sound should only be applicable for when Gamepad or keyboard inputs
             // are active - more on this later
             if (!isClickHandled && !Application.isMobilePlatform && playerItemView?.Controller != null)
@@ -409,6 +82,8 @@ namespace UltimateFootballSystem.Gameplay.Tactics
 
         public override void OnPointerDown(PointerEventData eventData)
         {
+            if (!interactable) return;
+
             base.OnPointerDown(eventData);
             pointerDownPos = eventData.position;
             pointerDownTime = Time.time;
@@ -445,14 +120,19 @@ namespace UltimateFootballSystem.Gameplay.Tactics
         
         public void OnPointerClick(PointerEventData eventData)
         {
-            if (!isDragging)
-            {
-                HandleItemSelection();
-            }
+            if (!interactable || isDragging) return;
+
+            HandleItemSelection();
         }
 
         private void HandleItemSelection()
         {
+            if (!interactable)
+            {
+                Debug.Log($"[PlayerItemViewSelectionLayer] Selection blocked - item not selectable: {playerItemView?.Profile?.Name ?? "Unknown Player"}");
+                return;
+            }
+
             if (playerItemView?.mainView?.ViewMode == PlayerItemViewModeOption.Roles)
             {
                 if (playerItemView.Controller?.roleSelectorDialog != null)
@@ -497,7 +177,60 @@ namespace UltimateFootballSystem.Gameplay.Tactics
             }
         }
 
-    
-        // ... (any other existing methods)
+        #region Selectability Control
+
+        /// <summary>
+        /// Sets the selectability state of this player item
+        /// </summary>
+        /// <param name="selectable">True to enable selectability, false to disable</param>
+        public void SetSelectability(bool selectable)
+        {
+            interactable = selectable;
+
+            if (selectable)
+            {
+                // Restore normal visual state
+                var colors = this.colors;
+                colors.disabledColor = colors.normalColor;
+                this.colors = colors;
+            }
+            else
+            {
+                // Apply disabled visual state (semi-transparent)
+                var colors = this.colors;
+                colors.disabledColor = new Color(colors.normalColor.r, colors.normalColor.g, colors.normalColor.b, 0.5f);
+                this.colors = colors;
+            }
+
+            string playerName = playerItemView?.Profile?.Name ?? "Unknown Player";
+            Debug.Log($"[PlayerItemViewSelectionLayer] Selectability {(selectable ? "enabled" : "disabled")} for {playerName}");
+        }
+
+        /// <summary>
+        /// Enables selectability for this player item
+        /// </summary>
+        public void EnableSelectability() => SetSelectability(true);
+
+        /// <summary>
+        /// Disables selectability for this player item
+        /// </summary>
+        public void DisableSelectability() => SetSelectability(false);
+
+        /// <summary>
+        /// Toggles the selectability state
+        /// </summary>
+        /// <param name="forceState">Optional: force a specific state instead of toggling</param>
+        public void ToggleSelectability(bool? forceState = null)
+        {
+            bool newState = forceState ?? !interactable;
+            SetSelectability(newState);
+        }
+
+        /// <summary>
+        /// Gets the current selectability state
+        /// </summary>
+        public bool IsSelectable => interactable;
+
+        #endregion
     }
 }
